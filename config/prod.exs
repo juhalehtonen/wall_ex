e61e30fe8f1d72671389e677362e287e113099a0
Juha Lehtonen <juha.t.lehtonen.91@gmail.com>
2018-04-22 21:10:41 +0300

    Update origin check

--- config/prod.exs
+++ config/prod.exs
@@ -30,7 +30,8 @@ config :wall_ex, WallExWeb.Endpoint,
   url: [scheme: "https", host: configure_prod_url, port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  check_origin: ["//wallex*.herokuapp.com"]
 
 # Do not print debug messages in production
 config :logger, level: :info
