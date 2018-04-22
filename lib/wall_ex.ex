ac9102cadc2c1fae961d3bbe792a2b09b51d6f6f
Juha Lehtonen <juha.t.lehtonen.91@gmail.com>
2018-04-22 20:14:10 +0300

    Move configuration function to WallEx module

--- lib/wall_ex.ex
+++ lib/wall_ex.ex
@@ -6,4 +6,13 @@ defmodule WallEx do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  def configure_production_url do
+    app_name = System.get_env("HEROKU_APP_NAME")
+
+    case String.contains?(app_name, "-pr-") do
+      false -> "wallex.herokuapp.com"
+      true -> app_name <> ".herokuapp.com"
+    end
+  end
 end
