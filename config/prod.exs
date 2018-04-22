24cdaf6861fe72b2abb0ecf0a94cd1bbded6e458
Juha Lehtonen <juha.t.lehtonen.91@gmail.com>
2018-04-22 21:24:43 +0300

    asdk

--- config/prod.exs
+++ config/prod.exs
@@ -27,7 +27,6 @@ end
 # which you typically run after static files are built.
 config :wall_ex, WallExWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: configure_prod_url, port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
