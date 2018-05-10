30336a752a84f1b6b00b2fb24f3e064b23dd56e6
Juha Lehtonen <juha.t.lehtonen.91@gmail.com>
2018-05-10 13:10:35 +0300

    Periodic checking of expiration via Worker

--- lib/wall_ex/worker.ex
+++ lib/wall_ex/worker.ex
@@ -1,5 +1,10 @@
 defmodule WallEx.Worker do
+  @moduledoc """
+  Worker GenServer responsible for initializing ETS-based storage and running a
+  periodical check for expiring drawings.
+  """
   use GenServer
+  alias WallEx.Storage
 
   def start_link(state \\ []) do
     GenServer.start_link(__MODULE__, state, name: __MODULE__)
@@ -9,7 +14,19 @@ defmodule WallEx.Worker do
   GenServer.init/1 callback. Used to create the Storage (ETS table) on startup.
   """
   def init(state) do
-    WallEx.Storage.init()
+    Storage.init()
+    schedule_expiration()
     {:ok, state}
+  end
+
+  def handle_info(:expiration, state) do
+    IO.puts("Clearing expired drawings...")
+    Storage.delete_expiring()
+    schedule_expiration()
+    {:noreply, state}
+  end
+
+  defp schedule_expiration() do
+    Process.send_after(self(), :expiration, 5_000)
   end
 end
