081735179e06370087811612769d49b048c8cce5
Juha <juhalehtonen@users.noreply.github.com>
2020-04-30 22:24:42 +0300

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule WallEx.Mixfile do
   def application do
     [
       mod: {WallEx.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
